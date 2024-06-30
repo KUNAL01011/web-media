a7cb720e67e4c6511e37667cf6bb1882977f1444
Kunal Kumar <kunal34255@gmail.com>
2024-06-30 15:14:19 +0530

    basic setup done

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
+import connectMongoDB from './db/connectMongoDB.js';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use("/api/auth",authRouter)
+
+app.listen(PORT, ()=> {
+    console.log("Server is running on port 8000");
+    connectMongoDB();
+})
