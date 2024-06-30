cc6e7a9464b990b1a39117f766751cdeb1e28853
Kunal Kumar <kunal34255@gmail.com>
2024-06-30 20:25:15 +0530

    signup

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use("/api/auth",authRouter)
 
 app.listen(PORT, ()=> {
