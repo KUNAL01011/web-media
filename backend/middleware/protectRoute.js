a3207c7cdb25e0c859c3ebdbaae525c87e33965e
Kunal Kumar <kunal34255@gmail.com>
2024-07-05 17:06:10 +0530

    multer set

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -9,6 +9,7 @@ export const protectRoute = async (req, res, next) => {
 		}
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+	console.log(decoded)
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
 import {
-	commentOnPost,
-	createPost,
-	deletePost,
-	getAllPosts,
-	getFollowingPosts,
-	getLikedPosts,
-	getUserPosts,
-	likeUnlikePost,
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
 } from "../controllers/post.controller.js";
+import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
@@ -17,7 +18,17 @@ router.get("/all", protectRoute, getAllPosts);
 router.get("/following", protectRoute, getFollowingPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
-router.post("/create", protectRoute, createPost);
+router.post(
+  "/create",
+  protectRoute,
+  upload.fields([
+    {
+      name: "img",
+      maxCount: 1,
+    },
+  ]),
+  createPost
+);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
